e636d98f92a2daa375ae9d7af536524cdf70339e
Leo Ware <54528853+leo-ware@users.noreply.github.com>
2021-11-28 20:45:31 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
-# setup virtual environment
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-
 # configure notebook metadata
 python -c "
 import json
@@ -11,6 +6,11 @@ notebook['metadata']['author'] = input('Author: ')
 notebook['metadata']['title'] = input('Title: ')
 json.dump(notebook, open('notebook.ipynb', 'w'))
 "
+
+# setup virtual environment
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 
 # setup git
 git init
